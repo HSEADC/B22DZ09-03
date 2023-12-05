71f299561716ba5b5075e43fc18410066f1cd7a9
EkaterinaGomzyakova <kate.gm717@gmail.com>
2023-12-05 07:15:01 +0300

    add

--- webpack.common.js
+++ webpack.common.js
@@ -8,7 +8,8 @@ const path = require('path')
 
 module.exports = {
   entry: {
-    index: './src/index.js'
+    index: './src/index.js',
+    theory: './src/theory.js'
   },
   output: {
     filename: '[name].[contenthash].js',
@@ -94,46 +95,60 @@ module.exports = {
       chunkFilename: '[id].[contenthash].css'
     }),
 
-    // Index
+    // Index chunk
     new HtmlWebpackPlugin({
       template: './src/index.html',
-      filename: './index.html'
+      filename: './index.html',
+      chunks: ['index']
+    }),
+    //Theory chunk
+    new HtmlWebpackPlugin({
+      template: './src/theory.html',
+      filename: './theory.html',
+      chunks: ['theory'] // Дублируем имя Chunks в массив, чтоб он подгружал
     }),
 
     // Section
     new HtmlWebpackPlugin({
       template: './src/about.html',
-      filename: './about.html'
+      filename: './about.html',
+      chunks: ['index']
     }),
     new HtmlWebpackPlugin({
       template: './src/places.html',
-      filename: './places.html'
+      filename: './places.html',
+      chunks: ['index']
     }),
 
     new HtmlWebpackPlugin({
       template: './src/legends.html',
-      filename: './legends.html'
+      filename: './legends.html',
+      chunks: ['index']
     }),
 
     new HtmlWebpackPlugin({
       template: './src/characters.html',
-      filename: './characters.html'
+      filename: './characters.html',
+      chunks: ['index']
     }),
 
     // Article
     new HtmlWebpackPlugin({
       template: './src/places/metro.html',
-      filename: './places/metro.html'
+      filename: './places/metro.html',
+      chunks: ['index']
     }),
 
     new HtmlWebpackPlugin({
       template: './src/legends/metro_2.html',
-      filename: './legends/metro_2.html'
+      filename: './legends/metro_2.html',
+      chunks: ['index']
     }),
 
     new HtmlWebpackPlugin({
       template: './src/characters/sculpture_dog.html',
-      filename: './characters/sculpture_dog.html'
+      filename: './characters/sculpture_dog.html',
+      chunks: ['index']
     }),
     // Partials
     new HtmlWebpackPartialsPlugin([
